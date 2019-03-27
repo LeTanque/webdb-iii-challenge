7e482b2873d5abb4016216f6308e5f797b3752e6
Frank Martinez <frankie.martinez@gmail.com>
2019-03-27 14:09:02 -0700

    read some knex documentation. Improved notes about knex cli commands in index.js

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ const helmet = require('helmet');
 const knex = require('knex');
 
 
-// yarn knex init                        // creates config file empty
-// yarn knex migrate:make roles_table    // makes a table called roles
-// yarn knex migrate:latest              // creates latest migration
-// yarn knex migrate:rollback            //
+// Knex CLI commands
+// knex is not installed globally so all commands are prepended with yarn or npx
+//
+// Migrations allow for you to define sets of schema changes so upgrading a database is a breeze.
+// yarn knex init                               // creates config file empty
+// yarn knex migrate:make roles_table           // makes a migration roles_table
+// yarn knex migrate:latest                     // updates to latest migration
+// yarn knex migrate:rollback                   // Rollback migration
+// yarn knex migrate:make fix_roles_latinName   // makes a migration called fix_roles_latinName
+//
+// Seed files allow you to populate your database with test or seed data independent of your migration files.
+// yarn knex seed:make seed_name                // makes a seed file called seed_name
+// yarn knex seed:run                           // Runs seed files, populates you db with seed data
+
+
 // Knex configuration. Then, assign it to 'db', invoking the config through knex
 const knexConfig = {
     client: 'sqlite3',
     connection: {
       filename: './data/lambda.sqlite3',
     },
-    useNullAsDefault: true,     // needed for sqlite
-    debug: true,                // Useful for development
+    useNullAsDefault: true,             // needed for sqlite
+    debug: true,                        // Useful for development
 };
 const db = knex(knexConfig);
 
--- migrations/20190327132012_cohort_table.js
+++ migrations/20190327132012_cohort_table.js
@@ -1,16 +1,12 @@
 // What new changes we need to make
 exports.up = function(knex) {
-    return knex.schema.createTable('cohort', tbl => {
-        // primary key. Called ID and make it auto increment. Method for that.
-        tbl.increments('id'); // Id by default, or whatever you pass in.
-        tbl.string('name', 128).notNullable().unique()
-    })
+
 };
 
 
 // how to undo the changes made in the up function
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('cohort')
+
 };
 
 
